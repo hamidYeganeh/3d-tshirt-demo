62d6dd15b1e932d0d1d0832071157950f4d54c68
Hamid Yeganeh <hamidyeganeh82@gmail.com>
2025-05-31 04:12:55 +0330

    up

--- src/store.js
+++ src/store.js
@@ -2,10 +2,11 @@ import { proxy } from 'valtio'
 
 const state = proxy({
   intro: false,
-  colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934', '#dafdaf'],
-  decals: ['react', 'three2', 'pmndrs', 'spexup'],
+  colors: ['#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934', '#dafdaf'],
+  decals: ['react_thumb.png', 'three2_thumb.png', 'pmndrs_thumb.png', 'texture-1.webp', 'rusted-1.jpg'],
   color: '#EFBD4E',
-  decal: 'three2'
+  decal: 'three2',
+  heartShape: true
 })
 
 export { state }
